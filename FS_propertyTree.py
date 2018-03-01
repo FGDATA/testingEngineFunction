fd58994df76f7b81c9aa28fe4d8ae3266d573c1b
Israel Hernandez <israel.a.hernandez@gmail.com>
2018-03-01 14:06:15 -0800

    Engine testing

---
+++ FS_propertyTree.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+class Engine:
+    """initializes an Engine"""
+    def __init__(self):
+        self.reverser=0
+        self.throtle_rev=0
+        self.reverser_angle_rad=0
+
+    """For testing purposes prints the engine current values"""
+    def __str__(self):
+        return """The engine reverser value is %f, 
+               throtle reverser is %f, 
+               and reverser angle is %f rads""" % (self.reverser, self.throtle_rev, self.reverser_angle_rad)
---
+++ testing_engines.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+from FS_propertyTree import Engine
+     ##Now the Engine Class has been imported.
+
+class Engines727(Engine):  
+    """The rev thrus function"""
+    def toggleFastRevThrust(self):
+        self.reverser=1
+        self.throtle_rev=0.5
+        self.reverser_angle_rad=3.14
+
+###########TESTING################
+
+print """
+Testing Begins by creating the Engine Stack\n
+"""
+##Create the stack of 3 engines for B727
+engines=[Engines727() for engines in range(3)]
+#Show them
+for myEngine in engines:
+    print myEngine
+
+print """
+__________________________________________________________________\n
+Testing Continues by modifying the throtle_rev of the second engine to 0.25 specifically\n
+The Engine 1 values now are:
+"""
+#modifying and showing the second engine (engines[1]) of the stack
+engines[1].throtle_rev=0.25
+print engines[1]
+
+print """
+__________________________________________________________\n
+Testing ends by sending the toggleFastRevThrust function to all the engines of the stack\n
+"""
+for myEngine in engines:
+    myEngine.toggleFastRevThrust()
+    print myEngine
+
+
+
